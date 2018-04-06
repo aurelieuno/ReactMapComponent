a9e94dc6c3a9b95ce56073be34917ebbe0ab74af
Aurelie Lebec <aurelie.lebec@ge.com>
2018-04-06 16:46:51 -0500

    [spy test]

--- test/MapComponent.test.js
+++ test/MapComponent.test.js
@@ -1,11 +1,15 @@
 import MapComponent from '../app/components/map-comp/lib/MapComponent'
 import {MAP_API_KEY} from '../app/config'
 
+spy(MapComponent, 'componentDidMount')
+
 describe('Default MapComponent', () => {
   it('should render a default map with no zoom or style given', () => {
     const wrapper = mount(
       <MapComponent apiKey={MAP_API_KEY} center={'City Park, New Orleans, LA'} />
     )
+    expect(MapComponent.componentDidMount.calledOnce).to.equal(true)
+
     wrapper.unmount()
   })
 })
--- test/test-config.js
+++ test/test-config.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import chai, {expect} from 'chai'
 import {mount, shallow, configure} from 'enzyme'
+import {spy} from 'sinon'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({adapter: new Adapter()})
 
 global.expect = expect
+global.spy = spy
 global.mount = mount
 global.shallow = shallow
 global.React = React
